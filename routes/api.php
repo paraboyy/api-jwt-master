99f0a547e9ef60fc8bd7da41ce0a0375876b434b
adhim <adhimxtkj@gmail.com>
2024-05-08 00:14:07 +0800

    update api

--- routes/api.php
+++ routes/api.php
@@ -24,19 +24,19 @@ use App\Http\Controllers\OlahDataController;
 Route::post('login', [ApiController::class, 'authenticate']);
 Route::post('register', [ApiController::class, 'register']);
 
-Route::get('/data', [OlahDataController::class, 'index']);
-Route::get('/total-customer', [OlahDataController::class, 'getTotalCustomers']);
-Route::get('/total-sales', [OlahDataController::class, 'getTotalSales']);
-Route::get('/total-quantity', [OlahDataController::class, 'getTotalQuantity']);
-Route::get('/total-profit', [OlahDataController::class, 'getTotalProfit']);
-Route::get('/sales-pertahun', [OlahDataController::class, 'getTotalSalesPerYear']);
-Route::get('/sales-segment', [OlahDataController::class, 'getTotalSalesBySegment']);
-Route::get('/sales-shipmode', [OlahDataController::class, 'getTotalSalesByShipmode']);
-
 
 Route::group(['middleware' => ['jwt.verify']], function () {
     Route::get('logout', [ApiController::class, 'logout']);
     Route::get('getuser', [ApiController::class, 'get_user']);
+
+    Route::get('/data', [OlahDataController::class, 'index']);
+    Route::get('/total-customer', [OlahDataController::class, 'getTotalCustomers']);
+    Route::get('/total-sales', [OlahDataController::class, 'getTotalSales']);
+    Route::get('/total-quantity', [OlahDataController::class, 'getTotalQuantity']);
+    Route::get('/total-profit', [OlahDataController::class, 'getTotalProfit']);
+    Route::get('/sales-pertahun', [OlahDataController::class, 'getTotalSalesPerYear']);
+    Route::get('/sales-segment', [OlahDataController::class, 'getTotalSalesBySegment']);
+    Route::get('/sales-shipmode', [OlahDataController::class, 'getTotalSalesByShipmode']);
     
     # Route::get('/data', [OlahDataController::class, 'index']);
 });
